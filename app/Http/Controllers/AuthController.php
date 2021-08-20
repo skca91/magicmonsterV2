92da288d6c5073c7f20319c4a459e05d6eeea2d5
Stephanie Correa <stephanie.correa24@gmail.com>
2021-08-20 18:00:19 -0300

    agregado firebase al proyecto

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,11 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use App\Models\User;
-use Illuminate\Http\Request;
-use Carbon\Carbon;
-
+use Kreait\Firebase\Auth as Firebase;
+use Kreait\Firebase\ServiceAccount;
+use Kreait\Firebase\Factory;
 
 /**
 * @OA\Info(title="API Magic Monster", version="1.0")
@@ -15,5 +13,41 @@ use Carbon\Carbon;
 
 class AuthController extends Controller
 {
+
+    protected $auth;
     
+    function __construct(Firebase $auth)
+    {
+
+        $arrayServiceAccount = array(
+            "type"=>"service_account",
+            
+            "project_id" => env('FIREBASE_PROYECT_ID'),
+            "client_id" => env('FIREBASE_CLIENT_ID'),
+            "client_email" => env('FIREBASE_CLIENT_EMAIL'),
+            "private_key" => env('FIREBASE_PRIVATE_KEY')
+        );
+
+        $firebase = (new Factory)->withServiceAccount($arrayServiceAccount);
+        $this->auth = $firebase->createAuth();
+
+    	//$this->auth = $auth;
+    }
+
+    public function validarTokenUser(string $token)
+    {
+    	$AuthorizationToken=$token;
+    	try{
+    		$verifiedIdToken = $this->auth->verifyIdToken($AuthorizationToken);
+    	}catch (InvalidToken $e) {
+            return "Unauthenticated";
+        }
+         $uid = $verifiedIdToken->claims()->get('sub');
+         $exp = $verifiedIdToken->claims()->get('exp');
+
+        if($uid){
+                return $uid;
+        }
+        return 'Unauthenticated';
+    }
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,8 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
-use Laravel\Passport\Passport;
-use Laravel\Passport\PassportServiceProvider;
 
 
 class AuthServiceProvider extends ServiceProvider
@@ -28,7 +26,7 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        Passport::routes();
+        
         
     }
 }
